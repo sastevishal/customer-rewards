aadb4d3228a4c8d415443f00f1c6a8ec1ac53dee
Vishal Saste <sastevishal7@gmail.com>
2025-06-17 11:20:27 -0700

    Refactored reward service: improved validation, used Optional, added customer transactions, cleaned tests and package names
    # Conflicts:
    #	src/main/java/com/retail/handler/RewardValidationHandler.java
    #	src/main/java/com/retail/helper/RewardCalculatorHelper.java
    #	src/main/java/com/retail/service/RewardServiceImpl.java
    #	src/main/java/com/retail/util/DateValidator.java
    #	src/test/java/com/retail/controller/RewardControllerTest.java
    #	src/test/java/com/retail/service/RewardServiceImplTest.java
    #	src/test/java/com/retail/util/DateValidatorTest.java
    #	src/test/java/com/retail/util/RewardValidationUtilTest.java
    
    # Conflicts:
    #	src/main/java/com/retail/dto/CustomerRewardResponse.java

--- src/main/java/com/retail/dto/CustomerRewardResponse.java
+++ src/main/java/com/retail/dto/CustomerRewardResponse.java
@@ -30,16 +30,6 @@ public class CustomerRewardResponse {
 
     }
 
-    public CustomerRewardResponse(Long customerId, String customerName, int totalPoints) {
-        this.customerId = customerId;
-        this.customerName = customerName;
-        this.totalRewards = totalPoints;
-    }
-
-    public CustomerRewardResponse() {
-
-    }
-
     public Long getCustomerId() {
         return customerId;
     }
