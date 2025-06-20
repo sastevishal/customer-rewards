75141809e83c9f9b90221195544c70a4c41be968
Vishal Saste <sastevishal7@gmail.com>
2025-06-20 04:18:48 -0700

    Adjusted reward point calculation to round amounts: .5 and above rounds up, below .5 rounds down

--- src/main/java/com/retail/util/RewardUtil.java
+++ src/main/java/com/retail/util/RewardUtil.java
@@ -8,9 +8,12 @@ public class RewardUtil {
      * - 1 point for every dollar spent between $50 and $100.
      */
     public static int calculateRewardPoints(double amount) {
-        int intAmount = (int) Math.floor(amount+0.5);
-        int over100 = Math.max(0, intAmount - 100);
-        int between50And100 = Math.max(0, Math.min(intAmount, 100) - 50);
-        return over100 * 2 + between50And100;
+        if (amount > 100) {
+            return (int) Math.round(2 * (amount - 100) + 50);
+        } else if (amount > 50) {
+            return (int) Math.round(amount - 50);
+        } else {
+            return 0;
+        }
     }
 }
